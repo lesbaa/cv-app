5ab4868c7ca07c4c7b46d72efb8e69efe22b2460
Les Moffat <les@lesmoffat.co.uk>
2018-05-25 19:08:23 +0100

    express with http/2 is broken

--- server.js
+++ server.js
@@ -1,133 +1,101 @@
+/* eslint-env node */
 const express = require('express')
 const serveStatic = require('serve-static')
 const nextApp = require('next')
+const http2 = require('http2')
 const compression = require('compression')
-const PORT = require('./server/metro.server.config').expressPort || process.env.port || 8080
-const { BASE_SECTION_ROUTES } = require('./server/metro.routing.config')
-const handleEmbedIframe = require('./server/lib/handle-embed-iframe')
-const getSectionId = require('./server/lib/get-section-id')
-const getCustomApiNum = require('./server/lib/get-custom-api-num')
 
-// These are only used in a dev environment
+const logListener = require('./server/logListener')
 
-process.env.port = PORT
+const PORT = process.env.PORT || 8080
+process.env.PORT = PORT
+
+console.log(PORT)
 
 const dev =
-  process.env.NODE_ENV === 'local' ||
-  process.env.NODE_ENV === 'stv3'
+  process.env.NODE_ENV === 'dev' ||
+  process.env.NODE_ENV !== 'live'
 
-const useLocalHttps = Boolean(process.env.LOCAL_HTTPS)
+
 const useReduxDevTools = process.env.USE_REDUX_DEVTOOLS === '1'
-const app = nextApp({ dev })
-const handle = app.getRequestHandler()
+// const app = nextApp({ dev })
 
-const routeHandler = (req, res, next, pagePath) => {
-  const {
-    hostname,
-    originalUrl,
-    protocol,
-    query,
-    params: requestParams,
-  } = req
-  let {
-    baseSection,
-    subSection,
-    guid: paramGuid,
-  } = requestParams
+// const handle = app.getRequestHandler()
 
-  const apiTpNumber = getCustomApiNum(query, process.env.NODE_ENV)
-  const {
-    guid: queryGuid,
-    clearcache,
-    throwError,
-  } = query
-
-  const baseSectionDoesNotExistAsRoute = !BASE_SECTION_ROUTES.includes(baseSection)
-
-  if (!requestParams || baseSectionDoesNotExistAsRoute) return next()
-
-  const { parentSection } = getSectionId(subSection || baseSection)
-
-  const newQuery = {
-    baseSection,
-    subSection,
-    parentSection,
-    ...(useReduxDevTools && { useReduxDevTools }),
-    guid: paramGuid || queryGuid,
-    environment: process.env.NODE_ENV,
-    ...(clearcache && { clearcache }),
-    env: process.env.NODE_ENV,
-    baseUrl: [protocol, '://', hostname, PORT ? `:${PORT}` : '', originalUrl].join(''),
-    path: originalUrl,
-    apiTpNumber,
-    throwError,
-  }
-
-
-  const additionalQueryParams = { ...query, ...newQuery }
-  const isWeatherRoute = baseSection === 'weather'
-  const isErrorRoute = subSection === 'invoke-error'
-
-  let page = pagePath
-
-  if (isWeatherRoute) {
-    page = '/weather'
-  }
-  if (isErrorRoute) {
-    page = '/invoke-error'
-  }
-
-  app.render(req, res, page, additionalQueryParams)
-}
+// const routeHandler = (req, res, next, pagePath) => {
+//   app.render(req, res, pagePath, additionalQueryParams)
+// }
 
 const server = express()
 
-server.use(compression())
+// server.use(compression())
 // gzip all requests
 
 // the static assets
 server.use(
-  '/news/fonts',
+  '/static/fonts',
   serveStatic('static/fonts', {
     fallthrough: true,
     maxAge: 31536000,
   })
 )
+
 server.use(
-  '/news/',
-  serveStatic('static/', {
+  '/static',
+  serveStatic('static', {
     fallthrough: true,
     maxAge: 86400,
   })
 )
 
-app.prepare().then(() => {
-  server.get('/news/utils/embed-iframe', handleEmbedIframe)
+const docHead = `
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+<h1>Hiya!</h1>
+`
 
-  server.get(
-    ['/:baseSection/:subSection', '/:baseSection'],
-    (req, res, next) => routeHandler(req, res, next, '/index')
-  )
+server.get('/hey', (rq, rs, nxt) => {
+  rq.on('push', console.log)
+  rs.write(docHead)
+  const stream = rs.push('static/test.js', {
+    status: 200, // optional
+    method: 'GET', // optional
+    request: {
+      accept: '*/*',
+    },
+    response: {
+      'content-type': 'application/javascript',
+    },
+  })
 
-  server.get(
-    '/:baseSection/:subSection/:guid',
-    (req, res, next) => routeHandler(req, res, next, '/article')
-  )
+  stream.on('error', (err) => {
+    console.log('push error!', err)
+  })
 
-  // TODO this will change when assetPrefix support is added
-  server.get('*', (req, res) => handle(req, res))
+  stream.end('<script src="static/test.js"></script>')
 
-  if (useLocalHttps && dev) {
-    const fs = require('fs')
-    const https = require('https')
-    const privateKey = fs.readFileSync('server/dev-https/metro.dev.key', 'utf8')
-    const certificate = fs.readFileSync('server/dev-https/metro.dev.crt', 'utf8')
-    const credentials = { key: privateKey, cert: certificate }
-    const httpsServer = https.createServer(credentials, server)
-    httpsServer.listen(PORT)
-  } else {
-    server.listen(PORT)
-  }
+  rs.end('<script src="static/test.js"></script><img src="//lorempixel.com/200/200"></body></html>')
 
-  console.log(`Server running on ** ${process.env.NODE_ENV} ** environment and on port: ${PORT}`)
 })
+
+// app.prepare().then(() => {
+console.log(`Server running on ** ${process.env.NODE_ENV} ** environment and on port: ${PORT}`)
+
+const fs = require('fs')
+
+const cert = fs.readFileSync('server/dev-https/lescv.dev.crt', 'utf-8')
+const key = fs.readFileSync('server/dev-https/lescv.dev.key', 'utf-8')
+
+const options = {
+  key,
+  cert,
+}
+
+http2
+  .createSecureServer(options, server)
+  .listen(PORT, logListener('HTTP/2'))
+
+// })
