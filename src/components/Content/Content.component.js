7186494763b846c2af28b0b3e4a51eb5d10fea15
Les Moffat <les@lesmoffat.co.uk>
2018-05-28 17:26:20 +0100

    pass and use correct props in Content.component

--- src/components/Content/Content.component.js
+++ src/components/Content/Content.component.js
@@ -1,22 +1,30 @@
 import React from 'react'
-import getBlurbContent from '~/components/blurbs'
 import Main from '~/components/Main'
 import Title from '~/components/Title'
 import Nav from '~/components/Nav'
 import styles from './Content.styles'
 
 const Content = ({
-  slideName,
+  blurbMarkup,
   title,
+  nextSlide,
+  previousSlide,
+  palette,
 }) => (
   <div
     className="Content"
   >
-    <Title title={title} />
+    <Title title={title} opacity={palette.offsetOpacity} />
     <Main>
-      {getBlurbContent(slideName)}
+      <span
+        dangerouslySetInnerHTML={{ __html: blurbMarkup }} // eslint-disable-line
+        opacity={palette.offsetOpacity}
+      />
     </Main>
-    <Nav />
+    <Nav
+      nextSlide={nextSlide}
+      previousSlide={previousSlide}
+    />
     <style jsx>{styles}</style>
   </div>
 )
