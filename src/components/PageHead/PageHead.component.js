8f7b8a05dcf406284e4fdfab0f6678d2e0d42ffd
Les Moffat <les@lesmoffat.co.uk>
2018-05-28 17:27:32 +0100

    include transition styles in PageHead

--- src/components/PageHead/PageHead.component.js
+++ src/components/PageHead/PageHead.component.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Head from 'next/head'
 import reset from '~/theme/reset.styles'
+import transitionStyles from '~/theme/page-transitions.styles'
 
 const PageHead = ({
   title,
@@ -10,6 +11,7 @@ const PageHead = ({
       <title>{title}</title>
     </Head>
     <style jsx global>{reset}</style>
+    <style jsx global>{transitionStyles}</style>
   </div>
 )
 
