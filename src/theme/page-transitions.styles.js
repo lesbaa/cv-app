45dc67478eaae41783c537abffbab063ec964685
Les Moffat <les@lesmoffat.co.uk>
2018-05-28 17:22:55 +0100

    implement page-transitions globals styles

---
+++ src/theme/page-transitions.styles.js
@@ -0,0 +1,67 @@
+import css from 'styled-jsx/css'
+
+export default css`
+
+.page-transition-enter .Title,
+.page-transition-enter .gradient,
+.page-transition-enter .Main,
+.page-transition-enter .Nav {
+  transform: translateY(5vh);
+  opacity: 0;
+}
+
+.page-transition-enter-active .Title,
+.page-transition-enter-active .gradient,
+.page-transition-enter-active .Main,
+.page-transition-enter-active .Nav {
+  transform: translateY(0);
+  opacity: 1;
+  transition: opacity 250ms, transform 250ms;
+}
+
+.page-transition-exit .Title,
+.page-transition-exit .gradient,
+.page-transition-exit .Main,
+.page-transition-exit .Nav {
+  transform: translateY(0);
+  opacity: 1;
+}
+
+.page-transition-exit-active .Title,
+.page-transition-exit-active .gradient,
+.page-transition-exit-active .Main,
+.page-transition-exit-active .Nav {
+  transform: translateY(-5vh);
+  opacity: 0;
+  transition: opacity 250ms, transform 250ms;
+}
+
+.page-transition-enter .gradient {
+  transform: translateY(0);
+}
+.page-transition-enter-active .gradient {
+  transform: translateY(0);
+}
+.page-transition-exit .gradient {
+  transform: translateY(0);
+}
+.page-transition-exit-active .gradient {
+  transform: translateY(0);
+}
+
+.page-transition-exit-active .Title, 
+.page-transition-enter-active .Title {
+  transition-delay: 0ms
+}
+
+.page-transition-exit-active .Main, 
+.page-transition-enter-active .Main {
+  transition-delay: 100ms
+}
+
+.page-transition-exit-active .Nav, 
+.page-transition-enter-active .Nav {
+  transition-delay: 200ms
+}
+
+`
