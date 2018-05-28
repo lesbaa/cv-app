37bc2131937c7c1215e7b00508f97122ea4c2ae7
Les Moffat <les@lesmoffat.co.uk>
2018-05-28 17:24:36 +0100

    first attempt at a web component

---
+++ pages/_document.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import Document, { Head, Main, NextScript } from 'next/document'
+
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
+
+  render() {
+    return (
+      <html>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+          <script src="/static/customElements/index.js" />
+        </body>
+      </html>
+    )
+  }
+}
