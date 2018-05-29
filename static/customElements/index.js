fe809bea0fc4063bba5087b61da7db6086a65576
Les Moffat <les@lesmoffat.co.uk>
2018-05-29 21:11:10 +0100

    working x-les-anim shader

--- static/customElements/index.js
+++ static/customElements/index.js
@@ -7,31 +7,45 @@ const fragmentShader = `
 uniform float uTime;
 varying vec2 vTextureCoord;
 uniform sampler2D uSampler;
-
+#define THRESH 0.2
 void main() {
-  float sinT = sin(gl_FragCoord.x / 2.0 + uTime * 20.0);
-  float cosT = cos(gl_FragCoord.x / 2.0 + uTime * 20.0);
+  vec2 coord = vec2(vTextureCoord.x + .25, vTextureCoord.y / 2.0 + .6) / 2.5;
   
-  vec2 pointOne = vec2(-.5, sin(uTime - .5) / 5.0 + 0.6);
-  vec2 pointTwo = vec2(.0, sin(uTime) / 5.0 + 0.4);
-  vec2 pointThree = vec2(.5, sin(uTime + .5) / 5.0 + 0.6);
+  vec2 pointOne = vec2(0.59, sin(uTime - .5) / 15.0 + 0.25);
+  vec2 pointTwo = vec2(.44, sin(uTime / 2.0 -.25) / 10.0 + 0.4);
+  vec2 pointThree = vec2(.29, sin(uTime) / 15.0 + 0.25);
+  vec2 pointFour = vec2(.14, sin(uTime + .5) / 10.0 + 0.4);
+  vec2 pointFive = vec2(0.0, sin(uTime + .75) / 15.0 + 0.25);
   
   vec4 pixelData = texture2D(uSampler, vTextureCoord);
 
-  bool isWithinRange =
-    distance(vTextureCoord.xy, pointOne) < sinT / 5.0 ||
-    distance(vTextureCoord.xy, pointTwo) < cosT / 5.0 ||
-    distance(vTextureCoord.xy, pointThree) < sinT / 5.0;
+  float distOne = distance(coord.xy, pointOne);
+  float distTwo = distance(coord.xy, pointTwo);
+  float distThree = distance(coord.xy, pointThree);
+  float distFour = distance(coord.xy, pointFour);
+  float distFive = distance(coord.xy, pointFive);
 
-  float range = (
-    distance(vTextureCoord.xy, pointOne) +
-    distance(vTextureCoord.xy, pointTwo) +
-    distance(vTextureCoord.xy, pointThree)
+  float rangeR = (
+    distOne +
+    distTwo +
+    distThree
+  ) / 4.0;
+
+  float rangeG = (
+    distTwo +
+    distThree +
+    distFour
   ) / 3.0;
 
-  float r = distance(vTextureCoord.xy, pointOne) > 0.5 ? 1.0 : 0.4;
-  float g = distance(vTextureCoord.xy, pointTwo) > 0.5 ? 1.0 : 0.4;
-  float b = distance(vTextureCoord.xy, pointThree) > 0.5 ? 1.0 : 0.4;
+  float rangeB = (
+    distThree +
+    distFour +
+    distFive
+  ) / 3.0;
+
+  float r = rangeR > THRESH ? 1.0 : 0.2;
+  float g = rangeG > THRESH ? 1.0 : 0.2;
+  float b = rangeB > THRESH ? 1.0 : 0.2;
   float a = pixelData.a;
   
   gl_FragColor = vec4(
@@ -48,18 +62,43 @@ void main() {
 class LesAnim extends HTMLElement {
   constructor() {
     super()
+
     this.init = this.init.bind(this)
     this.render = this.render.bind(this)
+    this.loadPixiScript = this.loadPixiScript.bind(this)
+    this.initStyles = this.initStyles.bind(this)
+    this.initCanvas = this.initCanvas.bind(this)
+    this.initPixi = this.initPixi.bind(this)
+    this.initShader = this.initShader.bind(this)
+    this.initText = this.initText.bind(this)
+    this.initRender = this.initRender.bind(this)
+    this.init = this.init.bind(this)
 
-    const script = document.createElement('script')
-    script.addEventListener('load', this.init)
-    script.src = '//cdnjs.cloudflare.com/ajax/libs/pixi.js/4.8.0/pixi.min.js'
-
-    const shadow = this.attachShadow({ mode: 'open' })
-    shadow.appendChild(script)
+    this.init()
   }
 
-  init() {
+  loadPixiScript() {
+    return new Promise((resolve, reject) => {
+      const script = document.createElement('script')
+      script.addEventListener('load', resolve)
+
+      const shadow = this.attachShadow({ mode: 'open' })
+      shadow.appendChild(script)
+
+      script.src = '//cdnjs.cloudflare.com/ajax/libs/pixi.js/4.8.0/pixi.min.js'
+    })
+  }
+
+  async fetchFont() {
+    await fetch('/static/fonts/LeagueSpartan-Bold.subset.eot')
+    await fetch('/static/fonts/LeagueSpartan-Bold.subset.woff')
+    await fetch('/static/fonts/LeagueSpartan-Bold.subset.ttf')
+    return fetch('/static/fonts/LeagueSpartan-Bold.subset.svg')
+  }
+
+  async init() {
+    await this.loadPixiScript()
+    await this.fetchFont()
     this.initStyles()
     this.initCanvas()
     this.initPixi()
@@ -75,12 +114,14 @@ class LesAnim extends HTMLElement {
         0% { opacity: 0 }
         100% { opacity: 1 }
       }
-    
+
       .fade-in {
         animation: .3s .25s fade backwards ease-out;
       }
 
       .canvas {
+        font-family: 'LeagueSpartan';
+        font-weight: bolder;
         position: absolute;
         top: 0;
         left: 0;
@@ -132,10 +173,11 @@ class LesAnim extends HTMLElement {
       width,
       height,
     } = this.canvas.getBoundingClientRect()
-    const text = new PIXI.Text('Les.', {
+    const textString = this.childNodes[0].textContent
+    const text = new PIXI.Text(textString, {
+      fontFamily: 'LeagueSpartan',
       fontWeight: 'bold',
       fontSize: height,
-      fontFamily: 'LeagueSpartan',
       fill: '#333',
       align: 'center',
       stroke: '#FFFFFF',
@@ -161,6 +203,12 @@ class LesAnim extends HTMLElement {
     this.shader.uniforms.uTime += 0.01
     this.pixiApp.renderer.render(this.pixiApp.stage)
   }
+
+  disconnectedCallback() {
+    cancelAnimationFrame(this.render)
+  }
 }
 
 customElements.define('x-lesanim', LesAnim)
+
+// TODO fallback for this, inject css into something somewhere and render text as normal?
