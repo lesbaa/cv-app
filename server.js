465dc659a2f7289f7f4d3ae759e813753a17cec9
Les Moffat <les@lesmoffat.co.uk>
2018-08-28 15:28:54 +0100

    refactor or server routing to make code a little DRYer

--- server.js
+++ server.js
@@ -49,82 +49,58 @@ server.use(
 
 server.get('/calender', handleCalender)
 
+const nextAppHandler = pageComponent => async (req, res, next) => {
+  const cached = cache.get(req.originalUrl)
+  if (cached && req.query.nocache !== 'true' && !dev) {
+    res.set('X-cache', 'hit')
+    res.send(cached)
+    return
+  }
+
+  const { slidename = 'hello' } = req.params
+  const markup = await app.renderToHTML(
+    req,
+    res,
+    pageComponent,
+    {
+      ...req.query,
+      slidename,
+    }
+  )
+
+  cache.set(req.originalUrl, markup)
+  if (req.query && req.query.ref && !req.cookies) res.cookie('LES_REF', req.query.ref)
+  res.send(markup)
+}
+
+
 app.prepare().then(() => {
-  // TODO, this stuff could be a bit DRYer
-  server.get('*', async (req, res, next) => {
+  server.get('*', (req, res, next) => {
     if (isMobile(req) && !req.path.includes('_next')) {
-      const cached = cache.get(req.originalUrl)
-      if (cached && req.query.nocache !== 'true' && !dev) {
-        res.set('X-cache', 'hit')
-        res.send(cached)
-        return
-      }
-
-      const markup = await app.renderToHTML(req, res, '/mobile', { ...req.query })
-      cache.set(req.originalUrl, markup)
-      res.send(markup)
+      return nextAppHandler('mobile')
     }
-    next()
+    return next()
   })
 
-  server.get('/', async (req, res, next) => {
-    const cached = cache.get(req.originalUrl)
-    if (cached && req.query.nocache !== 'true' && !dev) {
-      res.set('X-cache', 'hit')
-      res.send(cached)
-      return
-    }
-
-    const markup = await app.renderToHTML(req, res, '/index', { ...req.query })
-    cache.set(req.originalUrl, markup)
-    res.send(markup)
-  })
-
-  server.get('/cv', async (req, res, next) => {
-    const cached = cache.get(req.originalUrl)
-
-    if (cached && req.query.nocache !== 'true' && !dev) {
-      res.set('X-cache', 'hit')
-      res.send(cached)
-      return
-    }
-
-    const markup = await app.renderToHTML(req, res, '/CVSlide', { ...req.query, slidename: 'hello' })
-
-    cache.set(req.originalUrl, markup)
-    res.send(markup)
-  })
-
-  server.get('/cv/:slidename', async (req, res, next) => {
-    const cached = cache.get(req.originalUrl)
-    if (cached && req.query.nocache !== 'true' && !dev) {
-      res.set('X-cache', 'hit')
-      res.send(cached)
-      return
-    }
-
-    const { slidename } = req.params
-    const markup = await app.renderToHTML(req, res, '/CVSlide', { ...req.query, slidename })
-
-    cache.set(req.originalUrl, markup)
-    res.send(markup)
-  })
+  server.get('/', nextAppHandler('/index'))
+  server.get('/cv', nextAppHandler('/CVSlide'))
+  server.get('/cv/:slidename', nextAppHandler('/CVSlide'))
 
   console.log(`Server running on ** ${process.env.NODE_ENV} ** environment and on port: ${PORT}`)
-
-  const fs = require('fs')
-
-  const cert = fs.readFileSync('server/dev-https/lescv.dev.crt', 'utf-8')
-  const key = fs.readFileSync('server/dev-https/lescv.dev.key', 'utf-8')
-
-  const options = {
-    key,
-    cert,
-  }
 
   server.get('*', handle)
 
   if (dev) {
+    const fs = require('fs')
+
+    const cert = fs.readFileSync('server/dev-https/lescv.dev.crt', 'utf-8')
+    const key = fs.readFileSync('server/dev-https/lescv.dev.key', 'utf-8')
+
+    const options = {
+      key,
+      cert,
+    }
+
     http2
       .createServer(options, server)
       .listen(PORT, logListener('HTTP/2'))
