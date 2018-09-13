04f355a8e06dcd41120f01c725e6c9edcde0b42c
Les Moffat <les@lesmoffat.co.uk>
2018-09-13 09:55:54 +0100

    clipboard include export from index

--- src/utils/clipboard/index.js
+++ src/utils/clipboard/index.js
@@ -1,32 +1,5 @@
-/* eslint-env browser */
-export function fallbackCopyTextToClipboard(text) {
-  const textArea = document.createElement('textarea')
-  textArea.value = text
-  document.body.appendChild(textArea)
-  textArea.focus()
-  textArea.select()
+import copyTextToClipboard from './copyTextToClipboard'
+import fallbackCopyTextToClipboard from './fallbackCopyTextToClipboard'
 
-  let success = false
-
-  try {
-    success = document.execCommand('copy')
-  } catch (err) {
-    console.error('Unable to copy to clipboard: ', err)
-  }
-
-  document.body.removeChild(textArea)
-  return success
-}
-
-export default async function copyTextToClipboard(text) {
-  if (!navigator.clipboard) {
-    return fallbackCopyTextToClipboard(text)
-  }
-  try {
-    await navigator.clipboard.writeText(text)
-    return true
-  } catch (err) {
-    console.log('Unable to copy to clipbpard: ', err)
-    return false
-  }
-}
+export default copyTextToClipboard
+export { fallbackCopyTextToClipboard }
