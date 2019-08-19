f5f97f1ec120759a1894f58ed16a8842a0a9013b
Les Moffat <les@lesmoffat.co.uk>
2019-08-19 10:51:24 +0100

    update outdated dep

--- les.config.js
+++ les.config.js
@@ -1,4 +1,5 @@
 module.exports = {
-  API_BASE_URL: 'api.lesmoffat.co.uk/api/v1',
+  // API_BASE_URL: '//api.lesmoffat.co.uk/api/v1',
+  API_BASE_URL: 'http://localhost:3000/api/v1',
   DIALOG_TIMEOUT: 3000,
 }
